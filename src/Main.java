be9cdee615375e33831372628c54f5bf53b3042d
Andrei <Zubanoff163@gmail.com>
2024-11-07 20:55:01 +0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Varlables {
+public class Main {
     public static void main(String[] args) {
-       int myInt = 557;
+        int myInt = 557;
         short myShort = 3266;
         long myLong = 24724824;
 
         double myDouble = 235.35;
         float myFloat = 2362.4f;
 
-        char c ='a';
+        char c = 'a';
         boolean b = true;
 
         byte myByte = 100; // -128-127
         System.out.println(myInt);
+    }
+}
